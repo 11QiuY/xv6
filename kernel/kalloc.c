1c660561a2206f454ca1a8cdeff1c22fcccec1b4
11QiuY <2596426263@qq.com>
2024-12-21 14:51:38 +0800

    Q1

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -11,72 +11,117 @@
 
 void freerange(void *pa_start, void *pa_end);
 
-extern char end[]; // first address after kernel.
-                   // defined by kernel.ld.
+extern char end[];  // first address after kernel.
+                    // defined by kernel.ld.
 
 struct run {
   struct run *next;
 };
 
-struct {
+struct Kmem {
   struct spinlock lock;
-  struct run *freelist;
-} kmem;
+  struct run     *freelist;
+};
 
-void
-kinit()
-{
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+struct Kmem kmems[NCPU];
+
+int get_cpuid() {
+  push_off();
+  int cpu_id = cpuid();
+  pop_off();
+  return cpu_id;
 }
 
-void
-freerange(void *pa_start, void *pa_end)
-{
+void kinit() {
+  // initlock(&kmem.lock, "kmem");
+  // 为每个CPU初始化一个kmem
+  for (int i = 0; i < NCPU; i++) {
+    char buf[8];
+    snprintf(buf, 8, "kmem%d", i);
+    initlock(&kmems[i].lock, "kmem");
+  }
+  freerange(end, (void *)PHYSTOP);
+}
+
+void freerange(void *pa_start, void *pa_end) {
   char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+  p = (char *)PGROUNDUP((uint64)pa_start);
+  for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE) kfree(p);
 }
 
 // Free the page of physical memory pointed at by pa,
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
-void
-kfree(void *pa)
-{
+void kfree(void *pa) {
   struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+  if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+  int cpu_id = get_cpuid();
+  r = (struct run *)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  acquire(&kmems[cpu_id].lock);
+  r->next = kmems[cpu_id].freelist;
+  kmems[cpu_id].freelist = r;
+  release(&kmems[cpu_id].lock);
 }
+
+#define MAX_STEAL_TIMES 0
 
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
-void *
-kalloc(void)
-{
+void *kalloc(void) {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  int cpu_id = get_cpuid();
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+  acquire(&kmems[cpu_id].lock);
+  r = kmems[cpu_id].freelist;
+  if (r)
+    kmems[cpu_id].freelist = r->next;
+  else {  // 询问其他CPU是否有空闲内存
+    for (int i = 0; i < NCPU; i++) {
+      int find = 0;
+      if (i == cpu_id) continue;
+      push_off();
+      acquire(&kmems[i].lock);
+
+      r = kmems[i].freelist;
+      if (r) {  // 窃取内存
+        find = 1;
+        kmems[i].freelist = r->next;
+        kmems[cpu_id].freelist = r;
+        kmems[cpu_id].freelist->next = 0;
+
+        for (int j = 0; j < MAX_STEAL_TIMES - 1;
+             j++) {  // 尝试窃取MAX_STEAL_TIMES次
+
+          r = kmems[i].freelist;
+          if (r) {
+            kmems[i].freelist = r->next;
+            r->next = kmems[cpu_id].freelist;
+            kmems[cpu_id].freelist = r;
+          } else {
+            break;
+          }
+        }
+      }
+      release(&kmems[i].lock);
+      pop_off();
+      if (find) break;
+    }
+    // 在窃取完所有CPU的内存后，再次尝试分配
+    r = kmems[cpu_id].freelist;
+    if (r) kmems[cpu_id].freelist = r->next;
+  }
+  release(&kmems[cpu_id].lock);
+
+  if (r) memset((char *)r, 5, PGSIZE);  // fill with junk
+  return (void *)r;
 }
