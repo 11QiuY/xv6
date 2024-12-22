e4676fa5611c9f58e6ac2b90f5e315c65890fcab
11QiuY <2596426263@qq.com>
2024-12-22 18:53:16 +0800

    完结撒花

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -5,62 +5,51 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sleeplock.h"
+#include "fs.h"
+#include "fcntl.h"
+#include "file.h"
 
-uint64
-sys_exit(void)
-{
+extern void mmapunmap(pagetable_t pagetable, uint64 va, uint64 npages,
+                      struct mmap_VMA *v);
+
+uint64 sys_exit(void) {
   int n;
   argint(0, &n);
   exit(n);
   return 0;  // not reached
 }
 
-uint64
-sys_getpid(void)
-{
-  return myproc()->pid;
-}
+uint64 sys_getpid(void) { return myproc()->pid; }
 
-uint64
-sys_fork(void)
-{
-  return fork();
-}
+uint64 sys_fork(void) { return fork(); }
 
-uint64
-sys_wait(void)
-{
+uint64 sys_wait(void) {
   uint64 p;
   argaddr(0, &p);
   return wait(p);
 }
 
-uint64
-sys_sbrk(void)
-{
+uint64 sys_sbrk(void) {
   uint64 addr;
-  int n;
+  int    n;
 
   argint(0, &n);
   addr = myproc()->sz;
-  if(growproc(n) < 0)
-    return -1;
+  if (growproc(n) < 0) return -1;
   return addr;
 }
 
-uint64
-sys_sleep(void)
-{
-  int n;
+uint64 sys_sleep(void) {
+  int  n;
   uint ticks0;
 
   argint(0, &n);
-  if(n < 0)
-    n = 0;
+  if (n < 0) n = 0;
   acquire(&tickslock);
   ticks0 = ticks;
-  while(ticks - ticks0 < n){
-    if(killed(myproc())){
+  while (ticks - ticks0 < n) {
+    if (killed(myproc())) {
       release(&tickslock);
       return -1;
     }
@@ -70,9 +59,7 @@ sys_sleep(void)
   return 0;
 }
 
-uint64
-sys_kill(void)
-{
+uint64 sys_kill(void) {
   int pid;
 
   argint(0, &pid);
@@ -81,13 +68,137 @@ sys_kill(void)
 
 // return how many clock tick interrupts have occurred
 // since start.
-uint64
-sys_uptime(void)
-{
+uint64 sys_uptime(void) {
   uint xticks;
 
   acquire(&tickslock);
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+static struct mmap_VMA *get_vma(uint64 addr, uint64 len, struct proc *p) {
+  for (int i = 0; i < MAX_VMA; i++) {
+    if (p->vma[i].valid == 0) continue;
+    if (p->vma[i].addr <= addr && p->vma[i].addr + p->vma[i].len >= addr) {
+      return &p->vma[i];
+    }
+  }
+  return 0;
+}
+
+// 从用户地址空间找到一段连续的虚拟内存地址,并且addr与len都要是PGROUNDUP的
+static void get_addr_len(uint64 *addr, uint64 len, struct proc *p) {
+  // 检查能否向右端插入长度为len的虚拟内存
+  uint64 begin = MMAPEND;
+  for (int i = 0; i < MAX_VMA; i++) {
+    if (p->vma[i].valid == 0) continue;
+    uint64 start = p->vma[i].addr;
+    uint64 end = start + p->vma[i].len;
+    uint64 rightsize = MMAPEND - end;
+    begin = begin < start ? begin : start;
+    if (rightsize <= len) continue;
+    for (int j = 0; j < MAX_VMA; j++) {
+      if (p->vma[j].valid == 0 || i == j) continue;
+      uint64 start2 = p->vma[j].addr;
+      uint64 r;
+      if ((r = start2 - end) > 0) {
+        rightsize = r < rightsize ? r : rightsize;
+      }
+    }
+    if (rightsize >= len) {
+      *addr = end;
+      return;
+    }
+  }
+  // 向左插入
+  *addr = (begin - len);
+}
+
+// void *mmap(void *addr, size_t len, int prot, int flags, int fd, off_t
+// offset);
+uint64 sys_mmap(void) {
+  uint64 addr;
+  size_t len;
+  int    prot;
+  int    flags;
+  int    fd;
+  off_t  offset;
+
+  argaddr(0, &addr);
+  argaddr(1, (uint64 *)&len);
+  argint(2, &prot);
+  argint(3, &flags);
+  argint(4, &fd);
+  argint(5, (int *)&offset);
+
+  struct proc *p = myproc();
+  struct file *f = p->ofile[fd];
+
+  if ((!f->readable && (prot & PROT_READ)) ||
+      (!f->writable && (prot & PROT_WRITE) && !(flags & MAP_PRIVATE))) {
+    return -1;
+  }
+
+  // we assume the addr is always 0 which means the kernel will decide the
+  // address
+  if (addr != 0) panic("mmap: addr is not 0");
+  // find a free virtual address space for mmap
+  len = PGROUNDUP(len);
+  get_addr_len(&addr, len, p);
+
+  for (int i = 0; i < MAX_VMA; i++) {
+    if (p->vma[i].valid == 0) {
+      p->vma[i].addr = addr;
+      p->vma[i].len = len;
+      p->vma[i].prot = prot;
+      p->vma[i].flags = flags;
+      if (f == 0) {
+        return -1;
+      }
+      filedup(f);
+      p->vma[i].file = f;
+      p->vma[i].offset = offset;
+      p->vma[i].valid = 1;
+      return addr;
+    }
+  }
+  return -1;
+}
+// int   munmap(void *addr, size_t len);
+uint64 sys_munmap(void) {
+  uint64 addr;
+  size_t len;
+
+  argaddr(0, &addr);
+  argaddr(1, (uint64 *)&len);
+
+  addr = PGROUNDDOWN(addr);
+  len = PGROUNDUP(len);
+
+  struct proc *p = myproc();
+
+  struct mmap_VMA *v = get_vma(addr, len, p);
+  if (v == 0) {
+    printf("addr = %p, len = %d\n", (void *)addr, (int)len);
+    printf("v not found\n");
+    return -1;
+  };
+  if (addr > v->addr && addr + len < v->addr + v->len) {
+    printf("inside address");
+    return -1;
+  }
+
+  mmapunmap(p->pagetable, addr, len / PGSIZE, v);
+
+  if (addr == v->addr) {
+    v->offset += len;
+    v->addr += len;
+  }
+  v->len -= len;
+  if (v->len == 0) {
+    fileclose(v->file);
+    v->valid = 0;
+  }
+  return 0;
 }
--- kernel/trap.c
+++ kernel/trap.c
@@ -5,9 +5,13 @@
 #include "spinlock.h"
 #include "proc.h"
 #include "defs.h"
+#include "fcntl.h"
+#include "sleeplock.h"
+#include "fs.h"
+#include "file.h"
 
 struct spinlock tickslock;
-uint ticks;
+uint            ticks;
 
 extern char trampoline[], uservec[], userret[];
 
@@ -16,45 +20,33 @@ void kernelvec();
 
 extern int devintr();
 
-void
-trapinit(void)
-{
-  initlock(&tickslock, "time");
-}
+void trapinit(void) { initlock(&tickslock, "time"); }
 
 // set up to take exceptions and traps while in the kernel.
-void
-trapinithart(void)
-{
-  w_stvec((uint64)kernelvec);
-}
+void trapinithart(void) { w_stvec((uint64)kernelvec); }
 
 //
 // handle an interrupt, exception, or system call from user space.
 // called from trampoline.S
 //
-void
-usertrap(void)
-{
+void usertrap(void) {
   int which_dev = 0;
 
-  if((r_sstatus() & SSTATUS_SPP) != 0)
-    panic("usertrap: not from user mode");
+  if ((r_sstatus() & SSTATUS_SPP) != 0) panic("usertrap: not from user mode");
 
   // send interrupts and exceptions to kerneltrap(),
   // since we're now in the kernel.
   w_stvec((uint64)kernelvec);
 
   struct proc *p = myproc();
-  
+
   // save user program counter.
   p->trapframe->epc = r_sepc();
-  
-  if(r_scause() == 8){
+
+  if (r_scause() == 8) {
     // system call
 
-    if(killed(p))
-      exit(-1);
+    if (killed(p)) exit(-1);
 
     // sepc points to the ecall instruction,
     // but we want to return to the next instruction.
@@ -65,20 +57,89 @@ usertrap(void)
     intr_on();
 
     syscall();
-  } else if((which_dev = devintr()) != 0){
+  } else if ((which_dev = devintr()) != 0) {
     // ok
+  } else if (r_scause() == 0xd || r_scause() == 0xf ||
+             r_scause() == 0xc) {  // page fault
+    uint64 va = r_stval();
+    int    find = 0;
+    for (int i = 0; i < MAX_VMA; i++) {
+      // 在VMA中查找是否有mmap过
+      struct mmap_VMA *vma = &p->vma[i];
+      if (vma->valid == 0) continue;
+      if (va >= vma->addr && va < vma->addr + vma->len) {
+        // 如果是mmap的地址，那么就分配一个物理页
+        if ((r_scause() == 0xd && (vma->prot & PROT_READ) == 0) ||
+            (r_scause() == 0xc && (vma->prot & PROT_EXEC) == 0) ||
+            (r_scause() == 0xf && (vma->prot & PROT_WRITE) == 0)) {
+          printf("usertrap(): unexpected scause 0x%lx pid=%d\n", r_scause(),
+                 p->pid);
+          printf("            sepc=0x%lx stval=0x%lx\n", r_sepc(), r_stval());
+          setkilled(p);
+          break;
+        }
+        void *pa = kalloc();
+        if (pa == 0) {
+          printf("usertrap(): kalloc failed\n");
+          setkilled(p);
+          break;
+        }
+        memset((void *)pa, 0, PGSIZE);
+        int flag;
+        flag = PTE_U;
+        if (vma->prot & PROT_READ) flag |= PTE_R;
+        if (vma->prot & PROT_WRITE) flag |= PTE_W;
+        if (vma->prot & PROT_EXEC) flag |= PTE_X;
+        find = 1;
+        // 从文件中读取数据到物理页
+        if (vma->file != 0) {
+          struct file *f = vma->file;
+          if (f == 0) {
+            printf("usertrap(): file not found\n");
+            setkilled(p);
+            break;
+          }
+
+          begin_op();
+          ilock(f->ip);
+          int n = readi(f->ip, 0, (uint64)pa,
+                        (PGROUNDDOWN(va) - vma->addr + vma->offset), PGSIZE);
+          iunlock(f->ip);
+          end_op();
+
+          if (n < 0) {
+            kfree((void *)pa);
+            printf("usertrap(): fileread failed\n");
+            setkilled(p);
+            break;
+          }
+        }
+        if (mappages(p->pagetable, PGROUNDDOWN((uint64)va), PGSIZE, (uint64)pa,
+                     flag) != 0) {
+          kfree((void *)pa);
+          printf("usertrap(): mappages failed\n");
+          setkilled(p);
+          break;
+        }
+        break;
+      }
+    }
+    if (find == 0) {
+      printf("usertrap(): unexpected scause 0x%lx pid=%d\n", r_scause(),
+             p->pid);
+      printf("            sepc=0x%lx stval=0x%lx\n", r_sepc(), r_stval());
+      setkilled(p);
+    }
   } else {
     printf("usertrap(): unexpected scause 0x%lx pid=%d\n", r_scause(), p->pid);
     printf("            sepc=0x%lx stval=0x%lx\n", r_sepc(), r_stval());
     setkilled(p);
   }
 
-  if(killed(p))
-    exit(-1);
+  if (killed(p)) exit(-1);
 
   // give up the CPU if this is a timer interrupt.
-  if(which_dev == 2)
-    yield();
+  if (which_dev == 2) yield();
 
   usertrapret();
 }
@@ -86,9 +147,7 @@ usertrap(void)
 //
 // return to user space
 //
-void
-usertrapret(void)
-{
+void usertrapret(void) {
   struct proc *p = myproc();
 
   // we're about to switch the destination of traps from
@@ -102,18 +161,18 @@ usertrapret(void)
 
   // set up trapframe values that uservec will need when
   // the process next traps into the kernel.
-  p->trapframe->kernel_satp = r_satp();         // kernel page table
-  p->trapframe->kernel_sp = p->kstack + PGSIZE; // process's kernel stack
+  p->trapframe->kernel_satp = r_satp();          // kernel page table
+  p->trapframe->kernel_sp = p->kstack + PGSIZE;  // process's kernel stack
   p->trapframe->kernel_trap = (uint64)usertrap;
-  p->trapframe->kernel_hartid = r_tp();         // hartid for cpuid()
+  p->trapframe->kernel_hartid = r_tp();          // hartid for cpuid()
 
   // set up the registers that trampoline.S's sret will use
   // to get to user space.
-  
+
   // set S Previous Privilege mode to User.
   unsigned long x = r_sstatus();
-  x &= ~SSTATUS_SPP; // clear SPP to 0 for user mode
-  x |= SSTATUS_SPIE; // enable interrupts in user mode
+  x &= ~SSTATUS_SPP;  // clear SPP to 0 for user mode
+  x |= SSTATUS_SPIE;  // enable interrupts in user mode
   w_sstatus(x);
 
   // set S Exception Program Counter to the saved user pc.
@@ -122,7 +181,7 @@ usertrapret(void)
   // tell trampoline.S the user page table to switch to.
   uint64 satp = MAKE_SATP(p->pagetable);
 
-  // jump to userret in trampoline.S at the top of memory, which 
+  // jump to userret in trampoline.S at the top of memory, which
   // switches to the user page table, restores user registers,
   // and switches to user mode with sret.
   uint64 trampoline_userret = TRAMPOLINE + (userret - trampoline);
@@ -131,28 +190,25 @@ usertrapret(void)
 
 // interrupts and exceptions from kernel code go here via kernelvec,
 // on whatever the current kernel stack is.
-void 
-kerneltrap()
-{
-  int which_dev = 0;
+void kerneltrap() {
+  int    which_dev = 0;
   uint64 sepc = r_sepc();
   uint64 sstatus = r_sstatus();
   uint64 scause = r_scause();
-  
-  if((sstatus & SSTATUS_SPP) == 0)
-    panic("kerneltrap: not from supervisor mode");
-  if(intr_get() != 0)
-    panic("kerneltrap: interrupts enabled");
 
-  if((which_dev = devintr()) == 0){
+  if ((sstatus & SSTATUS_SPP) == 0)
+    panic("kerneltrap: not from supervisor mode");
+  if (intr_get() != 0) panic("kerneltrap: interrupts enabled");
+
+  if ((which_dev = devintr()) == 0) {
     // interrupt or trap from an unknown source
-    printf("scause=0x%lx sepc=0x%lx stval=0x%lx\n", scause, r_sepc(), r_stval());
+    printf("scause=0x%lx sepc=0x%lx stval=0x%lx\n", scause, r_sepc(),
+           r_stval());
     panic("kerneltrap");
   }
 
   // give up the CPU if this is a timer interrupt.
-  if(which_dev == 2 && myproc() != 0)
-    yield();
+  if (which_dev == 2 && myproc() != 0) yield();
 
   // the yield() may have caused some traps to occur,
   // so restore trap registers for use by kernelvec.S's sepc instruction.
@@ -160,10 +216,8 @@ kerneltrap()
   w_sstatus(sstatus);
 }
 
-void
-clockintr()
-{
-  if(cpuid() == 0){
+void clockintr() {
+  if (cpuid() == 0) {
     acquire(&tickslock);
     ticks++;
     wakeup(&ticks);
@@ -181,33 +235,30 @@ clockintr()
 // returns 2 if timer interrupt,
 // 1 if other device,
 // 0 if not recognized.
-int
-devintr()
-{
+int devintr() {
   uint64 scause = r_scause();
 
-  if(scause == 0x8000000000000009L){
+  if (scause == 0x8000000000000009L) {
     // this is a supervisor external interrupt, via PLIC.
 
     // irq indicates which device interrupted.
     int irq = plic_claim();
 
-    if(irq == UART0_IRQ){
+    if (irq == UART0_IRQ) {
       uartintr();
-    } else if(irq == VIRTIO0_IRQ){
+    } else if (irq == VIRTIO0_IRQ) {
       virtio_disk_intr();
-    } else if(irq){
+    } else if (irq) {
       printf("unexpected interrupt irq=%d\n", irq);
     }
 
     // the PLIC allows each device to raise at most one
     // interrupt at a time; tell the PLIC the device is
     // now allowed to interrupt again.
-    if(irq)
-      plic_complete(irq);
+    if (irq) plic_complete(irq);
 
     return 1;
-  } else if(scause == 0x8000000000000005L){
+  } else if (scause == 0x8000000000000005L) {
     // timer interrupt.
     clockintr();
     return 2;
@@ -215,4 +266,3 @@ devintr()
     return 0;
   }
 }
-
